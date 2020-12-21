8fd1926042625c81bddb095877842baff74f15db
Lucas <laguero90@gmail.com>
2020-12-21 19:25:48 -0300

    Weather app v2

---
+++ build/precache-manifest.4d5c223e24e65acc0d63180cc401ef25.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "911b4ffa1f8add2556769366d27876e1",
+    "url": "/react_weatherapp/index.html"
+  },
+  {
+    "revision": "29eac38d7aff22122d5a",
+    "url": "/react_weatherapp/static/css/main.65c6dab7.chunk.css"
+  },
+  {
+    "revision": "2808b7636bc8d152630b",
+    "url": "/react_weatherapp/static/js/2.a347b4f8.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/react_weatherapp/static/js/2.a347b4f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29eac38d7aff22122d5a",
+    "url": "/react_weatherapp/static/js/main.320cdbc7.chunk.js"
+  },
+  {
+    "revision": "9b4a2f4294d80ea9c80f",
+    "url": "/react_weatherapp/static/js/runtime-main.1d012e0f.js"
+  },
+  {
+    "revision": "b5d5d120eb2f2604e56ac60072c5299e",
+    "url": "/react_weatherapp/static/media/logoHome.b5d5d120.png"
+  }
+]);
